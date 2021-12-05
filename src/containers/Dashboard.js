c0cf97b6b9498fd7302e6af899d7b484348eb0ac
Yannick Aivayan <yannick.aivayan@visable.com>
2021-12-05 22:44:43 +0100

    fix(dashboard): handle big images in modal

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -76,10 +76,9 @@ export default class {
 
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr('data-bill-url')
-    const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
     $('#modaleFileAdmin1')
       .find('.modal-body')
-      .html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
+      .html(`<div class="modal-image-wrapper" style='text-align: center;'><img src=${billUrl} /></div>`)
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
