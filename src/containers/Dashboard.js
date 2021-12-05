a421fc551c3754befec9c970689c867db0f4960a
Yannick Aivayan <yannick.aivayan@visable.com>
2021-12-05 23:17:21 +0100

    refactor(jquery): don't use deprecated methods

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -67,9 +67,9 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.firestore = firestore
-    $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
-    $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
-    $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
+    $('#arrow-icon1').on(`click`, (e) => this.handleShowTickets(e, bills, 1))
+    $('#arrow-icon2').on(`click`, (e) => this.handleShowTickets(e, bills, 2))
+    $('#arrow-icon3').on(`click`, (e) => this.handleShowTickets(e, bills, 3))
     this.getBillsAllUsers()
     new Logout({ localStorage, onNavigate })
   }
@@ -102,9 +102,9 @@ export default class {
       $('.vertical-navbar').css({ height: '120vh' })
       this.counter++
     }
-    $('#icon-eye-d').click(this.handleClickIconEye)
-    $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
-    $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
+    $('#icon-eye-d').on(`click`, this.handleClickIconEye)
+    $('#btn-accept-bill').on(`click`, (e) => this.handleAcceptSubmit(e, bill))
+    $('#btn-refuse-bill').on(`click`, (e) => this.handleRefuseSubmit(e, bill))
   }
 
   handleAcceptSubmit = (e, bill) => {
