e14159848570023d403d27d93f2308c823bc4574
Yannick Aivayan <yannick.aivayan@visable.com>
2021-12-12 19:49:22 +0100

    test(new-bill): add for form upload

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,58 +1,120 @@
 import { fireEvent, screen, waitFor } from '@testing-library/dom'
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
 import NewBillUI from '../views/NewBillUI.js'
 import NewBill from '../containers/NewBill.js'
 
 describe('Given I am connected as an employee', () => {
-  const onNavigate = (pathname) => {
-    document.body.innerHTML = ROUTES({ pathname })
+  const onNavigate = jest.fn()
+  const FIRESTORE_URL = `/firestore/chuckNorris.png`
+  const getDownloadURL = jest.fn().mockResolvedValue(FIRESTORE_URL)
+  const storagePut = jest.fn().mockResolvedValue({ ref: { getDownloadURL } })
+  const storageRef = jest.fn().mockReturnValue({ put: storagePut })
+  const addBill = jest.fn().mockResolvedValue(true)
+  const bills = jest.fn().mockReturnValue({ add: addBill })
+  const firestore = {
+    storage: { ref: storageRef },
+    bills,
   }
+  const localStorageGetItem = jest.fn().mockReturnValue(JSON.stringify({ email: `johndoe@email.com` }))
+  const localStorage = { getItem: localStorageGetItem }
 
   beforeAll(() => {
     jest.spyOn(NewBill.prototype, `handleChangeFile`)
     jest.spyOn(NewBill.prototype, `uploadFile`)
+    jest.spyOn(NewBill.prototype, `getBill`)
     jest.spyOn(NewBill.prototype, `createBill`)
     jest.spyOn(NewBill.prototype, `handleSubmit`)
+  })
+
+  beforeEach(() => {
+    const html = NewBillUI()
+    document.body.innerHTML = html
   })
 
   afterEach(() => jest.clearAllMocks())
 
   describe('When I am on NewBill Page', () => {
-    test('Then I should be able to upload files', () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test(`Then I should be able to submit a valid form`, async () => {
       const newBill = new NewBill({
         document,
         onNavigate,
-        firestore: null,
-        localStorage: window.localStorage,
+        firestore,
+        localStorage,
+      })
+
+      fireEvent.change(screen.getByTestId(`expense-type`), { target: { value: `Transports` } })
+      fireEvent.change(screen.getByTestId(`expense-name`), { target: { value: `expense-name` } })
+      fireEvent.change(screen.getByTestId(`datepicker`), { target: { value: `2021-12-12` } })
+      fireEvent.change(screen.getByTestId(`amount`), { target: { value: `348` } })
+      fireEvent.change(screen.getByTestId(`vat`), { target: { value: `70` } })
+      fireEvent.change(screen.getByTestId(`file`), {
+        target: {
+          files: [new File(['(⌐□_□)'], 'chuckNorris.png', { type: 'image/png' })],
+        },
+      })
+      await waitFor(() => expect(newBill.fileUrl).toBe(FIRESTORE_URL))
+      fireEvent.submit(screen.getByTestId(`form-new-bill`))
+      expect(newBill.handleSubmit).toHaveBeenCalled()
+      expect(newBill.getBill).toHaveBeenCalled()
+      const EXPECTED_BILL = {
+        'amount': 348,
+        'commentary': '',
+        'date': '2021-12-12',
+        'email': 'johndoe@email.com',
+        'fileName': 'chuckNorris.png',
+        'fileUrl': '/firestore/chuckNorris.png',
+        'name': 'expense-name',
+        'pct': 20,
+        'status': 'pending',
+        'type': 'Transports',
+        'vat': 70,
+      }
+      expect(newBill.getBill).toHaveReturnedWith(EXPECTED_BILL)
+      expect(newBill.createBill).toHaveBeenCalled()
+      expect(bills).toHaveBeenCalled()
+      await waitFor(() => expect(addBill).toHaveBeenCalled())
+      expect(addBill).toHaveBeenCalledWith(EXPECTED_BILL)
+      await waitFor(() => expect(onNavigate).toHaveBeenCalled())
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Bills)
+    })
+
+    test('Then I should be able to upload files', async () => {
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage,
       })
 
       const $fileInput = screen.getByTestId(`file`)
       fireEvent.change($fileInput, {
         target: {
-          files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })],
+          files: [new File(['(⌐□_□)'], 'chuckNorris.png', { type: 'image/png' })],
         },
       })
       expect(newBill.handleChangeFile).toHaveBeenCalled()
       expect(newBill.uploadFile).toHaveBeenCalled()
+      expect(storageRef).toHaveBeenCalled()
+      await waitFor(() => expect(storagePut).toHaveBeenCalled())
+      await waitFor(() => expect(getDownloadURL).toHaveBeenCalled())
+      await waitFor(() => expect(newBill.fileUrl).toBe(FIRESTORE_URL))
+      await waitFor(() => expect(newBill.fileName).toBe('chuckNorris.png'))
       // form is incomplete => not posting!
       expect(newBill.createBill).not.toHaveBeenCalled()
     })
 
-    test('Then I should be able to upload files from the wrong format', () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test(`Then I shouldn't be able to upload files from the wrong format`, () => {
       const newBill = new NewBill({
         document,
         onNavigate,
-        firestore: null,
-        localStorage: window.localStorage,
+        firestore,
+        localStorage,
       })
 
       const $fileInput = screen.getByTestId(`file`)
       fireEvent.change($fileInput, {
         target: {
-          files: [new File(['(⌐□_□)'], 'chucknorris.bmp', { type: 'image/bmp' })],
+          files: [new File(['(⌐□_□)'], 'chuckNorris.bmp', { type: 'image/bmp' })],
         },
       })
       expect(newBill.handleChangeFile).toHaveBeenCalled()
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -6,6 +6,7 @@ export default class NewBill {
     this.document = document
     this.onNavigate = onNavigate
     this.firestore = firestore
+    this.localStorage = localStorage
     const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
     formNewBill.addEventListener('submit', (event) => this.handleSubmit(event))
     const file = this.document.querySelector(`input[data-testid="file"]`)
@@ -18,7 +19,7 @@ export default class NewBill {
   handleChangeFile() {
     const $input = this.document.querySelector(`input[data-testid="file"]`)
     const file = $input.files[0]
-    const isValidType = /image\/(jpe?g|png)/.test(file.type)
+    const isValidType = /^image\/(jpe?g|png)$/.test(file.type)
     if (!isValidType) {
       this.fileUrl = null
       this.fileName = null
@@ -38,44 +39,50 @@ export default class NewBill {
         this.fileUrl = url
         this.fileName = file.name
       })
+      .catch((error) => console.log(error))
   }
 
-  handleSubmit(event) {
-    event.preventDefault()
-    console.log(
-      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
-      event.target.querySelector(`input[data-testid="datepicker"]`).value,
-    )
-    const email = JSON.parse(localStorage.getItem('user')).email
-    const bill = {
-      email,
-      type: event.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name: event.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(event.target.querySelector(`input[data-testid="amount"]`, 10).value),
-      date: event.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: event.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(event.target.querySelector(`input[data-testid="pct"]`, 10).value) || 20,
-      commentary: event.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending',
+  getBill() {
+    try {
+      const $form = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+      // try catch to handle JSON.parse on localStorage
+      const user = JSON.parse(this.localStorage.getItem('user'))
+      const email = user?.email ?? false
+      if (!email) return
+      return {
+        email,
+        type: $form.querySelector(`select[data-testid="expense-type"]`).value,
+        name: $form.querySelector(`input[data-testid="expense-name"]`).value,
+        amount: parseInt($form.querySelector(`input[data-testid="amount"]`).value, 10),
+        date: $form.querySelector(`input[data-testid="datepicker"]`).value,
+        vat: parseInt($form.querySelector(`input[data-testid="vat"]`).value, 10),
+        pct: parseInt($form.querySelector(`input[data-testid="pct"]`).value, 10) || 20,
+        commentary: $form.querySelector(`textarea[data-testid="commentary"]`).value,
+        fileUrl: this.fileUrl,
+        fileName: this.fileName,
+        status: 'pending',
+      }
+    } catch (error) {
+      return {}
     }
-    const MANDATORY_KEYS = Object.keys(bill).filter((keyName) => keyName !== `commentary`)
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault()
+    const bill = this.getBill()
+    const MANDATORY_KEYS = Object.keys(bill).filter((keyName) => ![`pct`, `commentary`].includes(keyName))
     const isValidForm = MANDATORY_KEYS.every((keyName) => bill[keyName] != null && bill[keyName] !== ``)
     if (!isValidForm) return
-    this.createBill(bill)
+    await this.createBill(bill)
     this.onNavigate(ROUTES_PATH['Bills'])
   }
 
   // not need to cover this function by tests
   createBill(bill) {
     if (!this.firestore) return
-    this.firestore
+    return this.firestore
       .bills()
       .add(bill)
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
       .catch((error) => error)
   }
 }
